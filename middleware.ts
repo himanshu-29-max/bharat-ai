c6835b7fcb99a9ace0a2c08d01f4bad4423a82b1
himanshu-29-max <himanshu98015.rk@gmail.com>
2026-08-26 18:23:35 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,19 @@
-export { default } from "next-auth/middleware";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ["/"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
 };
